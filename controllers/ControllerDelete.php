dc9aefe2d9b1d2092786bda3e4b27d81a3391518
Isaque Menezes da Silva <isaque-menezes@hotmail.com>
2022-04-01 12:41:25 -0300

    Update ControllerDelete.php

--- controllers/ControllerDelete.php
+++ controllers/ControllerDelete.php
@@ -1,9 +1,7 @@
 <?php
     include "../core/config/config.php";
-    
-    $objEvents=new \Classes\ClassEvents();
-    $id=filter_input(INPUT_GET,'id',FILTER_DEFAULT);
 
-    $objEvents->delete_event(
-                            $id
-                            );
+    $objEvents = new \Classes\ClassEvents();
+    $id = filter_input(INPUT_GET, 'id', FILTER_DEFAULT);
+
+    $objEvents->delete_event($id);
